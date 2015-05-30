fe7dd55c1b60a657b244582b5e67c36221dc2d61
Jonathan Thomson <jonathan.mclean.thomson@gmail.com>
2015-05-30 20:43:21 +1200

    Add ccrx and Renesas License Manager installation untested

--- ccrx/recipes/default.rb
+++ ccrx/recipes/default.rb
@@ -6,3 +6,18 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe 'windows'
+
+windows_package 'CCRX V2.03.00' do
+  package_name 'Renesas RX Compiler V2.03.00'
+  source 'http://am.renesas.com/support/downloads/jq_download_category/ide/CC-RX_V20300_setup.exe'
+  installer_type :installshield
+  action :install
+end
+
+windows_package 'Renesas License Manager' do
+  package_name 'Renesas License Manager'
+  source 'http://am.renesas.com/support/downloads/jq_download_category/ide/LicenseManager_setup.exe'
+  installer_type :installshield
+  action  :install
+end
