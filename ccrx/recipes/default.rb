a5efcb5c9ef874333d0a9b861cb35ff4c99d3c3d
Jonathan Thomson <jonathan.mclean.thomson@gmail.com>
2015-05-31 18:41:55 +1200

    add ccrx install directory to system path

--- ccrx/recipes/default.rb
+++ ccrx/recipes/default.rb
@@ -10,14 +10,18 @@ include_recipe 'windows'
 
 windows_package 'CCRX V2.03.00' do
   package_name 'Renesas RX Compiler V2.03.00'
-  source 'http://am.renesas.com/support/downloads/jq_download_category/ide/CC-RX_V20300_setup.exe'
+  source 'c:/vagrant/CC-RX_V20300_setup.exe'
   installer_type :installshield
   action :install
 end
 
 windows_package 'Renesas License Manager' do
   package_name 'Renesas License Manager'
-  source 'http://am.renesas.com/support/downloads/jq_download_category/ide/LicenseManager_setup.exe'
+  source 'c:/vagrant/LicenseManager_setup.exe'
   installer_type :installshield
   action  :install
+end
+
+windows_path 'C:\Program Files (x86)\Renesas\RX\2_3_0\bin' do
+  action :add
 end
